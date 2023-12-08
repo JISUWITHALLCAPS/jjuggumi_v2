44b7a23628af189bacc903a258ff546cc2e9966a
JISUWITHALLCAPS <128721747+JISUWITHALLCAPS@users.noreply.github.com>
2023-12-08 23:13:52 +0900

    Add files via upload

---
+++ nightgame.c
@@ -0,0 +1,32 @@
+/*#define _CRT_SECURE_NO_WARNINGS
+#include "keyin.h"
+#include "jjuggumi.h"
+#include "canvas.h"
+#include <stdio.h>
+#include <string.h>
+void nightgame(void) {
+	while (1) {
+		if (player || (n_alive == 1)) {
+			break;
+		}
+		for (int i = 0; i < n_player; i++) {
+			for (int j = 0; j < n_item; j++) {
+				if (/) { // 플레이어 주변 탐색
+					if (player[i].hasitem == false) { //아이템 미보유 시
+						player[i].item = item[j];
+						item[j].intel_buf = false
+							player[i].hasitem = true;
+					}
+					else { //아이템 보유 시
+						if (아이템 보유 시 == true) {
+							for (int a = 0; a < n_item; a++) {
+								if (item[a]. == ) {}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+}*/
+
